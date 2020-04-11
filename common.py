75f70d69d1b8e5e6b85f38d51fa98be197271acf
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2020-04-11 17:35:30 +0300

    Moved TagsToHTML to common.py

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys, os, logging
+from PIL import Image
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -9,3 +10,107 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
+
+def getRes(imagePath):
+    try:
+        image = Image.open(imagePath)
+    except Exception as e:
+        logging.error(e)
+        return -1
+    size = image.size # returns tuple (x,y)
+    image.close()
+    x = size[0]
+    y = size[1]
+    while x > 600 or y > 600:
+        x -= 100
+        y -= 100
+    newSize = (x, y)
+
+    return newSize
+    
+
+def TagsToHTML(text, paths, preview, *args):
+    # **text** = <b></b> bolding
+    # __text__ = <i></i> italic
+    # ||text|| = <u></u> underlined
+    # @@link@@text@@ = <a href="">*text*</a> link
+    # $$img$$ = <p><img src="cid:0"></p> embedded image (cid:x number of image)
+    # TODO: Size option to images??
+    # Font is spesified to 'Calibri'
+
+    try:
+        images = args[0]
+    except IndexError as identifier:
+        pass
+    
+    start = '<html><body><font face="Calibri">'
+    end = '</font></body></html>'
+
+    # newline
+    text = "<p>" + text
+    text = text.replace('\n', '</p><p>')
+    text = "</p>" + text
+
+    #bolding
+    tempText = text
+    while True:
+        text = text.replace('**', '<b>', 1)
+        text = text.replace('**', '</b>', 1)
+        if text == tempText:
+            break
+        else:
+            tempText = text
+
+    #italic
+    tempText = text
+    while True:
+        text = text.replace('__', '<i>', 1)
+        text = text.replace('__', '</i>', 1)
+        if text == tempText:
+            break
+        else:
+            tempText = text
+    
+    #underlined
+    tempText = text
+    while True:
+        text = text.replace('||', '<u>', 1)
+        text = text.replace('||', '</u>', 1)
+        if text == tempText:
+            break
+        else:
+            tempText = text
+
+    #links
+    tempText = text
+    while True:
+        text = text.replace('@@', '<a href="', 1)
+        text = text.replace('@@', '">', 1)
+        text = text.replace('@@', '</a>', 1)
+        if text == tempText:
+            break
+        else:
+            tempText = text
+    
+    if text.find("$$img$$") != -1:
+        #embedded image
+        tempText = text
+        i = 0
+        for j in paths:
+            resolution = getRes(paths[i])
+            if resolution == -1:
+                return
+            if preview == 1:
+                text = text.replace('$$img$$', ('<img src="'+images[i]+'" alt="image" height="' + str(resolution[1]) + '" width="'+ str(resolution[0])+'">'), 1)
+            else:
+                kuva = j.split('/')
+                text = text.replace('$$img$$', ('<img src="cid:'+str(kuva[len(kuva)-1])+'" alt="kuva" height="' + str(resolution[1]) + '" width="'+ str(resolution[0])+'">'), 1)
+                logging.debug(text)
+            if text == tempText:
+                break
+            else:
+                tempText = text
+                i = i + 1
+
+    text = start + text + end
+    return text
--- masspost.py
+++ masspost.py
@@ -1,111 +1,7 @@
 import PySimpleGUI as sg
 import emailFunc as mail
-import logging, os, shutil
-from PIL import Image
+import logging, os, shutil, common
 
-def getRes(imagePath):
-    try:
-        image = Image.open(imagePath)
-    except Exception as e:
-        logging.error(e)
-        return -1
-    size = image.size # returns tuple (x,y)
-    image.close()
-    x = size[0]
-    y = size[1]
-    while x > 600 or y > 600:
-        x -= 100
-        y -= 100
-    newSize = (x, y)
-
-    return newSize
-    
-
-def TagsToHTML(text, paths, preview, *args):
-    # **text** = <b></b> bolding
-    # __text__ = <i></i> italic
-    # ||text|| = <u></u> underlined
-    # @@link@@text@@ = <a href="">*text*</a> link
-    # $$img$$ = <p><img src="cid:0"></p> embedded image (cid:x number of image)
-    # TODO: Size option to images??
-    # Font is spesified to 'Calibri'
-
-    try:
-        images = args[0]
-    except IndexError as identifier:
-        pass
-    
-    start = '<html><body><font face="Calibri">'
-    end = '</font></body></html>'
-
-    # newline
-    text = "<p>" + text
-    text = text.replace('\n', '</p><p>')
-    text = "</p>" + text
-
-    #bolding
-    tempText = text
-    while True:
-        text = text.replace('**', '<b>', 1)
-        text = text.replace('**', '</b>', 1)
-        if text == tempText:
-            break
-        else:
-            tempText = text
-
-    #italic
-    tempText = text
-    while True:
-        text = text.replace('__', '<i>', 1)
-        text = text.replace('__', '</i>', 1)
-        if text == tempText:
-            break
-        else:
-            tempText = text
-    
-    #underlined
-    tempText = text
-    while True:
-        text = text.replace('||', '<u>', 1)
-        text = text.replace('||', '</u>', 1)
-        if text == tempText:
-            break
-        else:
-            tempText = text
-
-    #links
-    tempText = text
-    while True:
-        text = text.replace('@@', '<a href="', 1)
-        text = text.replace('@@', '">', 1)
-        text = text.replace('@@', '</a>', 1)
-        if text == tempText:
-            break
-        else:
-            tempText = text
-    
-    if text.find("$$img$$") != -1:
-        #embedded image
-        tempText = text
-        i = 0
-        for j in paths:
-            resolution = getRes(paths[i])
-            if resolution == -1:
-                return
-            if preview == 1:
-                text = text.replace('$$img$$', ('<img src="'+images[i]+'" alt="image" height="' + str(resolution[1]) + '" width="'+ str(resolution[0])+'">'), 1)
-            else:
-                kuva = j.split('/')
-                text = text.replace('$$img$$', ('<img src="cid:'+str(kuva[len(kuva)-1])+'" alt="kuva" height="' + str(resolution[1]) + '" width="'+ str(resolution[0])+'">'), 1)
-                logging.debug(text)
-            if text == tempText:
-                break
-            else:
-                tempText = text
-                i = i + 1
-
-    text = start + text + end
-    return text
 
 def preview(text, images):
     folder = os.path.join((os.getenv("APPDATA") + "\\Haukiposti"), "images")
@@ -126,7 +22,7 @@ def preview(text, images):
             i += 1
             
     preview = 1
-    htmlText = TagsToHTML(text, images, preview, paths)
+    htmlText = common.TagsToHTML(text, images, preview, paths)
     if htmlText == -1:
         return -1
     try:
@@ -217,7 +113,7 @@ def massPost(configs, service):
                         sg.PopupOK("Liitteiden koko on suurempi kuin salittu 23 Mt.")
                     else:
                         text = values["messageText"]
-                        htmlText = TagsToHTML(text, attachements, preview=0)
+                        htmlText = common.TagsToHTML(text, attachements, preview=0)
                         receivers = CSVparser(values["receivers"])
                         if receivers:
                             encMsg = mail.createMail(configs[1], receivers, values["subject"], htmlText, attachements)
@@ -233,7 +129,7 @@ def massPost(configs, service):
                             sg.PopupOK("CSV tiedostoa lukiessa tapahtui virhe.")
                 else:
                     text = values["messageText"]
-                    htmlText = TagsToHTML(text, attachements, preview=0)
+                    htmlText = common.TagsToHTML(text, attachements, preview=0)
                     receivers = CSVparser(values["receivers"])
                     if receivers:
                         encMsg = mail.createMail(configs[1], receivers, values["subject"], htmlText, attachements)
