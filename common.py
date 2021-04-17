26c05f5694b6104f273ccb946fb7851f4ec66879
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-04-17 14:13:39 +0300

    Fixed errors in image adding and exiting

--- common.py
+++ common.py
@@ -337,7 +337,7 @@ def markdownParserHTML(text, paths, receiver, preview, *args):
         for j in paths:
             resolution = getRes(paths[i])
             if resolution == -1:
-                return
+                continue
             if preview == 1:
                 text = text.replace('$$img$$', ('<img src="'+images[i]+'" alt="image" height="' + str(resolution[1]) + '" width="'+ str(resolution[0])+'">'), 1)
             else:
